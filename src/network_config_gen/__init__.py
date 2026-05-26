4c2966634db075e904fc34a7430a1d4ceafc7db3
Josh <josh.carswell@thecarswells.ca>
2026-05-26 05:05:09 -0600

    feat: Add package initialization

---
+++ src/network_config_gen/__init__.py
@@ -0,0 +1,11 @@
+"""Network Configuration Generator - Main package."""
+
+__version__ = "0.1.0"
+__author__ = "Network Automation Team"
+__description__ = "Automated network switch configuration generation from ServiceNow CIs"
+
+__all__ = [
+    "cli",
+    "config",
+    "exceptions",
+]
